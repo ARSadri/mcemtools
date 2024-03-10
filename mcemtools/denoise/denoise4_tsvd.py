fixed annular masking for even-sized images

mcemtools/denoise/denoise4_tsvd.py
@@ -26,7 +26,8 @@ def denoise4_tsvd(
     criterion_I4D_LAGMUL,
     use_classes_by_scattering,
     use_repeat_by_scattering,
-    use_denoised_STEM,
+    use_pre_denoised_STEM,
+    STEM_denoiser_model_type,
     rank_info):
 
     FLAG_train_STEM, FLAG_train_I4D = include_training
@@ -50,9 +51,9 @@ def denoise4_tsvd(
     logged_ref     = logviewer(ref_dir)
     data4D_noisy   = logged_ref.get_single('noisy.npy')
     data4D_nonoise = logged_ref.get_single('nonoise.npy')
-    # if use_denoised_STEM:
+    # if use_pre_denoised_STEM:
     #     denoised_STEM  = logged_ref.get_single('denoised_STEM.npy')
-    #     assert denoised_STEM is not None, 'set use_denoised_STEM = False'
+    #     assert denoised_STEM is not None, 'set use_pre_denoised_STEM = False'
     
     data4D_shape = data4D_noisy.shape
     n_x, n_y, n_r, n_c = data4D_shape
@@ -97,20 +98,41 @@ def denoise4_tsvd(
     logger(f'data4D_nonoise.mean() : {data4D_nonoise.mean()}')
     logger(f'rank_info: {rank_info}')
     
+    from mcemtools.tensor_svd import scree_plots
+    screes = scree_plots(data4D_noisy.reshape(data4D_noisy.shape[0],data4D_noisy.shape[1],data4D_noisy.shape[2]*data4D_noisy.shape[3]))
+    plt.plot(screes[0], '-*', label = 'x direction')
+    plt.plot(screes[1], '-*', label = 'y direction')
+    plt.plot(screes[2], '-*', label = 'pixels')
+    plt.legend()
+    logger.log_plt('screes', dpi = 1000)
+    
+    logger.log_single('screes_0', screes[0])
+    logger.log_single('screes_1', screes[1])
+    logger.log_single('screes_2', screes[2])
+    
     err_all = []
-    for _n_pix in rank_info.n_pix:
-        denoised = mcemtools.tensor_svd.tensor_svd_denoise(
-            data4D_noisy, rank = (rank_info.n_x, rank_info.n_y, _n_pix))
-        denoised = denoised.reshape(*data4D_noisy_shape)
-        current_Err = ((denoised - data4D_nonoise)**2).mean()**0.5
-        err_all.append(current_Err)
+    _nx_ny_npix_all = []
+    for _n_x in rank_info.n_x:
+        for _n_y in rank_info.n_y:
+            for _n_pix in rank_info.n_pix:
+                print(f'{_n_x}/{rank_info.n_x[-1]}',
+                      f'{_n_y}/{rank_info.n_y[-1]}',
+                      f'{_n_pix}/{rank_info.n_pix[-1]}')
+                denoised = mcemtools.tensor_svd.tensor_svd_denoise(
+                    data4D_noisy, rank = (_n_x, _n_y, _n_pix))
+                denoised = denoised.reshape(*data4D_noisy_shape)
+                current_Err = ((denoised - data4D_nonoise)**2).mean()**0.5
+                err_all.append(current_Err)
+                _nx_ny_npix_all.append([_n_x, _n_y, _n_pix])
     err_all = np.array(err_all)
     logger(f'ranks are: {rank_info.n_pix}')
     logger(f'errors vs ranks are: {err_all}')
     ind = np.argmin(err_all)
-    logger(f'best rank is {ind}: {rank_info.n_pix[ind]}')
+    logger(f'best rank is {ind}: {_nx_ny_npix_all[ind]}')
+    _n_x, _n_y, _n_pix = _nx_ny_npix_all[ind]
+
     denoised = mcemtools.tensor_svd.tensor_svd_denoise(
-            data4D_noisy, rank = (rank_info.n_x, rank_info.n_y, rank_info.n_pix[ind]))
+            data4D_noisy, rank = (_n_x, _n_y, _n_pix))
     denoised = denoised.reshape(*data4D_noisy_shape)
     logger.log_single('I4D_denoiser/I4D_denoised/denoised', denoised)
     frame_denoised = mcemtools.data4D_to_frame(denoised)

mcemtools/denoise/networks.py
@@ -201,9 +201,6 @@ class U_Net(nn.Module):
 
         d1 = self.Conv_1x1(d2)
 
-        if(self.mask is not None):
-            d1[:, :, self.mask==0] = 0 
-
         d1 = d1 ** 2
         
         for dim in range(d1.shape[0]):
@@ -214,6 +211,10 @@ class U_Net(nn.Module):
                 d1[dim] *= self.mu_eaxct[inds[dim]]
             if(self.mu is not None):
                 d1[dim] *= self.mu[inds[dim]]
+
+        if(self.mask is not None):
+            d1[:, :, self.mask==0] = 0 
+        
         return d1
         
 

mcemtools/masking.py
@@ -40,12 +40,19 @@ def annular_mask(image_shape : tuple,
     """
     n_r, n_c = image_shape
     if centre is None: # use the middle of the image
-        centre = (int(n_r/2), int(n_c/2))
+        # centre = (int(n_r/2), int(n_c/2))
+        centre = (n_r/2, n_c/2)
     if radius is None: 
         # use the smallest distance between the centre and image walls
         radius = np.minimum(centre[0], centre[1])
 
     Y, X = np.ogrid[:n_r, :n_c]
+    
+    if n_r/2 == n_r//2:
+        Y = Y + 0.49
+    if n_c/2 == n_c//2:
+        X = X + 0.49
+    
     dist_from_centre = np.sqrt((X - centre[0])**2 + (Y-centre[1])**2)
 
     mask = dist_from_centre <= radius